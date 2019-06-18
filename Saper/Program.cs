bf66cffe37b6288bb81b3aacf843c0ab1a24c5d6
Darrefull <darrefull@gmail.com>
2019-06-18 22:18:14 +0200

    projekt

--- Saper/Program.cs
+++ Saper/Program.cs
@@ -333,7 +333,7 @@ namespace Saper {
             // Okienko formularza
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(442, 450);
+            this.ClientSize = new System.Drawing.Size(410, 440);
             this.Controls.Add(this.mines);
             this.Controls.Add(this.stan);
             this.Controls.Add(this.button_newgame);
